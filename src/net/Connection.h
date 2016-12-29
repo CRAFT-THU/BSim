7bffe129a5226b927dbe97f46bd836025bfb6932
yhx <shen_yhx@163.com>
2016-12-29 16:21:13 +0800

    backup

---
+++ src/net/Connection.h
@@ -0,0 +1,16 @@
+/* This header file is writen by qp09
+ * usually just for fun
+ * Thu December 29 2016
+ */
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+struct Connection {
+	int *pSynapsesNum; 
+	int *pSynapsesLoc; 
+	int *pSynapsesIdx; 
+	int synapsesNum; 
+};
+
+#endif /* CONNECTION_H */
+
