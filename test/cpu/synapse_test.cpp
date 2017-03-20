20545122b695f449d6f7eee6866fd44032b9d29c
yhx <shen_yhx@163.com>
2017-03-20 17:53:14 +0800

    modifed test

--- test/cpu/synapse_test.cpp
+++ test/cpu/synapse_test.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
 	Network c;
-	Population<IF_curr_exp> *pn1 = c.createPopulation(0, 1, IF_curr_exp(LIFNeuron(ID(0, 0), 0.0f, 0.0f, 0.0f, 1.0e-1f, 10.0e-3f, 0.0f, 1.0f, 1.0f, 15.0e-3f, 2.0e-3f), ID(0, nidTag.getTag())));
+	Population<IF_curr_exp> *pn1 = c.createPopulation(0, 1, IF_curr_exp(LIFNeuron(ID(0, 0), 0.0f, 0.0f, 0.0f, 1.0e-1f, 10.0e-3f, 0.0f, 1.0f, 1.0f, 15.0e-3f, 2.0e-1f), ID(0, nidTag.getTag())));
 	Population<IF_curr_exp> *pn2 = c.createPopulation(1, 1, IF_curr_exp(LIFNeuron(ID(0, 0), 0.0f, 0.0f, 0.0f, 1.0e-1f, 10.0e-3f, 0.0f, 1.0f, 1.0f, 15.0e-3f, 0), ID(0, nidTag.getTag())));
 
 	c.connect(pn1, pn2, 5.0e-4f, 1.0e-3f, Excitatory);
