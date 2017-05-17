3cc969484f7d1eaba789db19713323d8b055f61d
yhx13 <shen_yhx@163.com>
2017-05-17 04:04:49 -0400

    bash script update

--- build/bin/back.sh
+++ build/bin/back.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 mv Sim.log Sim.log.bak
+mv GSim.log GSim.log.bak
 mv GSim_sort.log GSim_sort.log.bak
-mv
+mv GSim.data GSim.data.bak
+
 mv v.data v.data.bak
 mv input_e.data input_e.data.bak
 mv input_i.data input_i.data.bak
 mv ie.data ie.data.bak
 mv ii.data ii.data.bak
-mv
+
 mv g_v.data g_v.data.bak
 mv g_input_e.data g_input_e.data.bak
 mv g_input_i.data g_input_i.data.bak
