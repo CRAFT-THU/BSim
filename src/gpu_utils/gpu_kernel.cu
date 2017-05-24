3fac330f034cf362ce3826c4ab042ced8805c86a
yhx13 <shen_yhx@163.com>
2017-05-24 10:07:06 -0400

    synapse method 2

--- src/gpu_utils/gpu_kernel.cu
+++ src/gpu_utils/gpu_kernel.cu
@@ -998,7 +998,8 @@ __global__ void update_pre_synapse(N2SConnection *pConnection)
 
 __global__ void update_dense_static_hit(GStaticSynapses *d_synapses, int num, int start_id)
 {
-#ifndef FAST_TEST
+#define FAST_TEST 2
+#if  FAST_TEST == 1
 	__shared__ int fire_neuron_id[MAXBLOCKSIZE];
 
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -1044,6 +1045,41 @@ __global__ void update_dense_static_hit(GStaticSynapses *d_synapses, int num, in
 			}
 			block_idx += gridDim.x;
 			__syncthreads();
+		}
+		__syncthreads();
+	}
+#elif FAST_TEST == 2
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+	for (int delta_t = 0; delta_t<MAX_DELAY; delta_t++) {
+		int block_idx = blockIdx.x;
+		int time_idx = (gCurrentIdx+MAX_DELAY-delta_t)%(MAX_DELAY+1);
+		int firedSize = gFiredTableSizes[time_idx];
+		int num_per_block = (firedSize - 1)/gridDim.x + 1;
+		int block_nums_minus_1 = (firedSize - 1) / num_per_block;
+
+		int fired_size_block = 0;
+		if (block_idx == block_nums_minus_1) {
+			fired_size_block = firedSize - block_idx * num_per_block;
+		} else if (block_idx < block_nums_minus_1) {
+			fired_size_block = num_per_block;
+		} else {
+			fired_size_block = 0;
+		}
+
+		for (int idx = 0; idx < fired_size_block; idx++) {
+			int nid = gFiredTable[time_idx*gFiredTableCap + (block_idx)*num_per_block + idx];
+			int start_loc = gConnection->delayStart[delta_t + nid * MAX_DELAY];
+			int synapseNum = gConnection->delayNum[delta_t + nid * MAX_DELAY];
+			for (int j=threadIdx.x; j<synapseNum; j += blockDim.x) {
+				//int sid = gConnection->pSynapsesIdx[j+start_loc];
+				int sid = j+start_loc;
+				real weight = d_synapses->p_weight[sid];
+				if (weight >= 0) {
+					atomicAdd(&(gNeuronInput[d_synapses->p_dst[sid]]), weight);
+				} else {
+					atomicAdd(&(gNeuronInput_I[d_synapses->p_dst[sid]]), weight);
+				}
+			}
 		}
 		__syncthreads();
 	}
