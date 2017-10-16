41f103c9309ae1863aa169b8ef151c962e92261b
yhx13 <shen_yhx@163.com>
2017-10-16 06:24:06 -0400

    sync

--- script/Model.py
+++ script/Model.py
@@ -1,37 +1,18 @@
 
 import re 
 
-#class Parameter:
-#
-#    def __init__(self, name, data_type):
-#        self.name = name
-#        self.type = data_type
-
 class Func:
     
-    def __init__(self, name):
+    def __init__(self, name, func_type):
         self.name = name
         self.output = {}
         self.input = {}
         self.operation = []
 
-def parase_para(content):
-    para = {}
-    for line in content:
-        line_obj = re.search('(\S[^#]*)#?', line)
-        if !line_obj:
-            continue
+    def load_func(content):
+        return None
 
-        line = line_obj.group(1)
 
-        para_obj = re.search('(\S*)\s*:\s*(\S.*)', line)
-
-        if !para_obj:
-            print "Parameter style wrong!"
-
-        para[para_obj.group(1)] = para_obj.group(2).split(', ')
-
-        return para
 
 class Model:
 
@@ -45,34 +26,65 @@ class Model:
     def load_para(content):
         self.para = parase_para(content)
 
+    def load_func(func):
+        self.func.append(func)
+
 
 
     def load_model(content):
-        content_offset = 0
-        offset_stack = []
-        content_list = []
+        blocks = parase_blocks(content)
 
-        last_obj = ''
+        for block in blocks:
 
-        for line in content:
 
-            line_obj = re.search('(\S[^#]*)#?', line)
-            if !line_obj:
+def parase_blocks(content):
+    blocks = {}
+    std_offset = -1
+    #offset_stack = []
+    block = []
+    block_meta = ''
+    for line in content:
+            line = re.search('([^#]*)', line).group(1)
+            line_obj = re.search('(^\s*([^#\s][^#]*[^#\s]))\s*', line)
+            if line_obj == None:
                 continue
             
             line = line_obj.group(1)
-            if re.match("@parameter", line)
-            #offset = line_obj.start()
-            #if offset > content_offset:
-            #    offset_stack.append(content_offset)
-            #    content_offset = offset
-            #elif offset < content_offset:
-            #    print content_list
-            #    content_list = []
-            #    while (offset < content_offset):
-            #        content_offset = offset_stack.pop()
-            #    
-            #content_list.append(line)
-                
+
+            offset = line_obj.start(2)
+            if (std_offset == -1):
+                std_offset = offset
+            if (offset > std_offset):
+                block.append(line)
+            elif (offset == std_offset):
+                if len(block) > 0:
+                    blocks[block_meta] = block
+                block_meta = line
+                block = []
+            else:
+                print "Wrong style: Outer block inside a block"
+
+    if len(block) > 0:
+        blocks[block_meta] = block
+
+    return blocks
 
 
+
+def parase_para(content):
+    para = {}
+    for line in content:
+        line_obj = re.search('(\S[^#]*)#?', line)
+        if not line_obj:
+            continue
+
+        line = line_obj.group(1)
+
+        para_obj = re.search('(\S*)\s*:\s*(\S.*)', line)
+
+        if not para_obj:
+            print "Parameter style wrong!"
+
+        para[para_obj.group(1)] = para_obj.group(2).split(', ')
+
+        return para
