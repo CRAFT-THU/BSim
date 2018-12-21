d8db2be4b5eecf719b55ca132b70f9d4c4e3b6c7
yhx <shen_yhx@163.com>
2018-12-22 01:53:24 +0800

    large net test

--- test/lif_test.py
+++ test/lif_test.py
@@ -27,28 +27,33 @@ class TestLIF(unittest.TestCase):
         net = Net(dt=0.0001)
         p0 = net.population(lif, num=N, name='P0', v=0.0, v_rest=0.0, v_reset=0.0,
                             c=0.1, tau=50e-3, tau_i=1.0, tau_e=1.0, v_threshold=15e-3,
-                            i_offset=1.0, refract_time=0.001, dt=0.001)
+                            i_offset=1.0, refract_time=0.00)
         p1 = net.population(lif, num=N, name='P1', v=0.0, v_rest=0.0, v_reset=0.0,
                             c=0.1, tau=50e-3, tau_i=1.0, tau_e=1.0, v_threshold=15e-3,
-                            i_offset=0.0, refract_time=0.001, dt=0.001)
+                            i_offset=0.0, refract_time=0.00)
         p2 = net.population(lif, num=N, name='P2', v=0.0, v_rest=0.0, v_reset=0.0,
                             c=0.1, tau=50e-3, tau_i=1.0, tau_e=1.0, v_threshold=15e-3,
-                            i_offset=0.0, refract_time=0.001, dt=0.001)
+                            i_offset=0.0, refract_time=0.00)
         p3 = net.population(lif, num=N, name='P3', v=0.0, v_rest=0.0, v_reset=0.0,
                             c=0.1, tau=50e-3, tau_i=1.0, tau_e=1.0, v_threshold=15e-3,
-                            i_offset=0.0, refract_time=0.001, dt=0.001)
+                            i_offset=0.0, refract_time=0.00)
         p4 = net.population(lif, num=N, name='P4', v=0.0, v_rest=0.0, v_reset=0.0,
                             c=0.1, tau=50e-3, tau_i=1.0, tau_e=1.0, v_threshold=15e-3,
-                            i_offset=0.0, refract_time=0.001, dt=0.001)
+                            i_offset=0.0, refract_time=0.00)
         p5 = net.population(lif, num=N, name='P5', v=0.0, v_rest=0.0, v_reset=0.0,
                             c=0.1, tau=50e-3, tau_i=1.0, tau_e=1.0, v_threshold=15e-3,
-                            i_offset=0.0, refract_time=0.001, dt=0.001)
+                            i_offset=0.0, refract_time=0.00)
 
-        s01 = net.projection(static, num=N*N, name='S01', weight=0.002, delay=0.001)
-        s12 = net.projection(static, num=N*N, name='S12', weight=0.002, delay=0.001)
-        s23 = net.projection(static, num=N*N, name='S23', weight=0.002, delay=0.001)
-        s34 = net.projection(static, num=N*N, name='S34', weight=0.002, delay=0.001)
-        s45 = net.projection(static, num=N*N, name='S45', weight=0.002, delay=0.001)
+        s01 = net.projection(static, num=N*N, name='S01', weight=0.002,
+                             delay=0.0001)
+        s12 = net.projection(static, num=N*N, name='S12', weight=0.002,
+                             delay=0.0001)
+        s23 = net.projection(static, num=N*N, name='S23', weight=0.002,
+                             delay=0.0001)
+        s34 = net.projection(static, num=N*N, name='S34', weight=0.002,
+                             delay=0.0001)
+        s45 = net.projection(static, num=N*N, name='S45', weight=0.002,
+                             delay=0.0001)
 
         net.all_to_all(p0, p1, s01)
         net.all_to_all(p1, p2, s12)
