7515a98872de833116aa826b39932144e81e38c3
Peng Qu <shen_yhx@163.com>
2018-12-24 10:36:55 +0800

    more complicate test

--- test/gpu/brian_test.cpp
+++ test/gpu/brian_test.cpp
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
 	const int N = 500;
 	Network c;
 	//createPopulation(int id, int N, LIFENeuron(ID id, real v_init, real v_rest, real v_reset, real cm, real tau_m, real tau_refrac, real tau_syn_E, real tau_syn_I, real v_thresh, real i_offset)), ID(0, 0), real tau_syn_E, real tau_syn_I);
-	Population<LIF_brian> *pn0 = c.createPopulation(0, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 10.0e-1), 1.0, 1.0));
-	Population<LIF_brian> *pn1 = c.createPopulation(1, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
+	Population<LIF_brian> *pn0 = c.createPopulation(0, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0002, 1.0, 1.0, 15.0e-3, 10.0e-1), 1.0, 1.0));
+	Population<LIF_brian> *pn1 = c.createPopulation(1, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0001, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
 	Population<LIF_brian> *pn2 = c.createPopulation(2, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
 	Population<LIF_brian> *pn3 = c.createPopulation(3, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
 	Population<LIF_brian> *pn4 = c.createPopulation(4, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
