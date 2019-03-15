06daa9d54da53e5f4d1d108b8eb4622ee38d91be
yhx <shen_yhx@163.com>
2019-03-15 11:29:56 +0800

    add test all

---
+++ build/bin/prof.sh
@@ -0,0 +1,2 @@
+
+nvprof --kernels update --metrics achieved_occupancy,branch_efficiency, global_hit_rate, local_hit_rate,sm_efficiency,warp_execution_efficiency,warp_nonpred_execution_efficiency  ./fx_test_all 300 1000 0.7 0.5 0.6 0.3 6 
